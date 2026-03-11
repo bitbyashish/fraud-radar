6ac33abe73a6ae198416c956b310277bbdd0bfb5
Ashish Tiwari <ashishtiwari7420@gmail.com>
2026-03-11 08:02:00 +0000

    feat: initialize Spring Boot project with Maven structure

---
+++ src/main/java/com/fraudradar/FraudRadarApplication.java
@@ -0,0 +1,13 @@
+package com.fraudradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudRadarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fraudradar/FraudRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fraudradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
